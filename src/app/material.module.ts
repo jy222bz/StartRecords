a6c7def624207d6bb78d5e6cf4933048248cac86
Niko <np222fc@student.lnu.se>
2019-04-11 12:30:28 +0200

    + add material module

---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@NgModule({
+    declarations: [],
+    imports: [MatToolbarModule],
+    providers: [],
+    bootstrap: []
+})
+export class MaterialModule {
+}
