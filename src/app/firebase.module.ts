13fa609d6b83e8f38e05de45e1acd585db4213e8
Jim Andersson <ja223rt@student.lnu.se>
2019-04-11 12:36:59 +0200

    + put firebase into firebase.module.ts

--- src/app/firebase.module.ts
+++ src/app/firebase.module.ts
@@ -1,10 +1,17 @@
 import {NgModule} from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 
 @NgModule({
     declarations: [],
-    imports: [],
+    imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireAuthModule,
+        AngularFirestoreModule
+    ],
     providers: [],
     bootstrap: []
 })
