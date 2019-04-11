4590c53b6cb901be8a3ccda3020523234a0c00c8
Osama Zarraa <osamaz26@gmail.com>
2019-04-11 12:24:29 +0200

    * fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { LoginComponent } from './components/login/login.component';
-
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireAuthModule } from '@angular/fire/auth';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {LoginComponent} from './components/login/login.component';
 
 
 @NgModule({
-<<<<<<< HEAD
-  declarations: [
-    AppComponent,
-    PageNotFoundComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-=======
     declarations: [
         AppComponent,
         PageNotFoundComponent,
-        ToolbarComponent
+        LoginComponent
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule,
-        AngularFireModule.initializeApp(environment.firebase),
-        AngularFireAuthModule,
-        AngularFirestoreModule
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
->>>>>>> 881ec7f8f114d08d840a18a468fc06d525b1b42e
 })
-export class AppModule { }
+export class AppModule {
+}
