bc5946a865c6e35152534af2a98276f3d7fd3347
Osama Zarraa <osamaz26@gmail.com>
2019-04-13 00:52:15 +0200

    + add authentication service + add categories service + add user model + add products service

---
+++ src/app/shared/services/categories.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+
+@Injectable()
+export class CategoriesService {
+
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    login() {
+
+    }
+
+    logout() {
+        
+    }
+}
