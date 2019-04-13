76cd332daf54192aa64bfa794e306d5aa7b61858
Osama Zarraa <osamaz26@gmail.com>
2019-04-13 02:59:13 +0200

    * disable routing until chunks are fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
+    /*
     {
         path: '',
         loadChildren: './modules/home/home.module#HomeModule?chunkName=home',
     },
+    */
     {
         path: 'admin/categories',
         loadChildren: './modules/admin/categories/categories.module#CategoriesModule?chunkName=categories',
