a0b0f7e37974109b70f12c910b4d4fff17fe302c
Osama Zarraa <osamaz26@gmail.com>
2019-04-13 14:43:57 +0200

    * fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {PageNotFoundComponent} from './shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-    /*
     {
         path: '',
-        loadChildren: './modules/home/home.module#HomeModule?chunkName=home',
+        loadChildren: './modules/home/home.module#HomeModule',
     },
-    */
     {
         path: 'admin/categories',
-        loadChildren: './modules/admin/categories/categories.module#CategoriesModule?chunkName=categories',
+        loadChildren: './modules/admin/categories/categories.module#CategoriesModule',
     },
     {path: '**', component: PageNotFoundComponent},
 ];
