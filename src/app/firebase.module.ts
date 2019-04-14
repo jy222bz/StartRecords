f5ac7ccf3eb77fcf3b59e2cd2afa2e92de43ed55
Osama Zarraa <osamaz26@gmail.com>
2019-04-15 01:50:23 +0200

    * add firebase modules to firebase.module

--- src/app/firebase.module.ts
+++ src/app/firebase.module.ts
@@ -1,9 +1,19 @@
 import {NgModule} from '@angular/core';
+import {AngularFireModule} from "@angular/fire";
+import {environment} from "../environments/environment";
+import {AngularFirestoreModule} from "@angular/fire/firestore";
+import {AngularFireAuthModule} from "@angular/fire/auth";
+import {AngularFireStorageModule} from "@angular/fire/storage";
 
 
 @NgModule({
     declarations: [],
-    imports: [],
+    imports: [
+        AngularFireModule.initializeApp(environment.firebase, 'my-app-name'),
+        AngularFirestoreModule,
+        AngularFireAuthModule,
+        AngularFireStorageModule,
+    ],
     providers: [],
     bootstrap: []
 })
