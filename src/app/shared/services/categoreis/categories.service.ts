2f776ba37c17e21754de3a0296c5cf33480c2089
qianxu <qx222ac@student.lnu.se>
2019-04-18 05:35:25 +0000

    Update categories.service.ts

--- src/app/shared/services/categoreis/categories.service.ts
+++ src/app/shared/services/categoreis/categories.service.ts
@@ -1,25 +1,30 @@
 import {Injectable} from '@angular/core';
-import {AngularFirestore} from "@angular/fire/firestore";
+import { Observable } from 'rxjs';
 import {Category} from "../../models/categories/category";
-
-
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 @Injectable()
 export class CategoriesService {
 
-
+    categories: Observable<Category[]>;
+    private categoryCollection: AngularFirestoreCollection<Category>;
     constructor(private afs: AngularFirestore) {
-
+        this.categoryCollection = afs.collection<Category>('categories');
+        this.categories = this.categoryCollection.valueChanges();
     }
 
     get(args = null) {
         return this.afs.collection<Category>('categories').valueChanges();
     }
 
-    add(args) {
 
+
+    add(category: Category) {
+
+        this.categoryCollection.add(category);
     }
 
-    delete(args) {
+    delete(category:Category) {
+
 
     }
 
