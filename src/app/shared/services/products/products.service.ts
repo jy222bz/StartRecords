83d1228fb71a54f8591275820efcd9e0152d8f57
qianxu <qx222ac@student.lnu.se>
2019-04-23 01:56:21 +0000

    *Prepare products service

--- src/app/shared/services/products/products.service.ts
+++ src/app/shared/services/products/products.service.ts
@@ -1,12 +1,23 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+
+import {AngularFireDatabase} from "@angular/fire/database";
+import {Product} from "../../models/products/product";
 
 
 @Injectable()
 export class ProductsService {
 
 
-    constructor(private http: HttpClient) {
+    constructor(private db: AngularFireDatabase) {
+
+    }
+
+    get() {
+        console.log('getting list');
+        return this.db.list<Product>('/products')
+    }
+
+    add(args) {
 
     }
 
