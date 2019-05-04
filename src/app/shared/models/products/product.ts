fa338873914a25945b6e239bbe9c422bb3ff66a9
Vilhelm Park <vp222dv@student.lnu.se>
2019-05-05 00:45:26 +0200

    Merge branches 'dev' and 'master' of https://gitlab.com/osamaz26/1dv508 into dev
    # Conflicts:
    #	src/app/shared/services/products/products.service.ts

--- src/app/shared/models/products/product.ts
+++ src/app/shared/models/products/product.ts
@@ -4,6 +4,7 @@ export class Product extends Model {
     name: string;
     artist: string;
     producer: string;
+    //year: string; TODO Fix year
     cover: string;
     price: number;
     total: number;
@@ -11,13 +12,14 @@ export class Product extends Model {
     description: string;
     createdAt: string;
 
-    constructor(id = '', name = '', producer = '', artist = '', price = 0, duration = 0, cover = '', description = '',
+    constructor(id = '', name = '', producer = '', artist = ''/*,year = ''*/, price = 0, duration = 0, cover = '', description = '',
                 total = 0, createdAt = null
     ) {
         super();
         this.id = id;
         this.name = name;
         this.artist = artist;
+        //this.year = year;
         this.price = price;
         this.total = total;
         this.duration = duration;
