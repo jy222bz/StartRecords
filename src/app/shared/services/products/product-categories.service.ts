69d39fa091649d70f790680857e7280f6630f1c0
Jim Andersson <ja223rt@student.lnu.se>
2019-05-05 10:53:05 +0200

    + update product cateogires service so it add and remove categories from a product

--- src/app/shared/services/products/product-categories.service.ts
+++ src/app/shared/services/products/product-categories.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
-import {Product} from "../../models/products/product";
-import {AngularFirestore} from "@angular/fire/firestore";
-
+import {AngularFirestore} from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
+import {Category} from '../../models/categories/category';
 
 @Injectable()
 export class ProductCategoriesService {
@@ -9,21 +9,95 @@ export class ProductCategoriesService {
 
     }
 
-    // array-contains
     get(id) {
-        return this.afs.collection<Product>('product_categories').doc(id).get();
+        return new Promise((resolve, reject) => {
+            this.afs.collection('products').doc(id).get()
+                .subscribe(
+                    (next) => {
+                        const subscription = this.afs.collection<Category>('categories').get().subscribe(
+                            (data) => {
+                                const res = [];
+                                data.docs.forEach((item) => {
+                                    next.data().categories.forEach((categoryId) => {
+                                        if (item.id === categoryId) {
+                                            res.push({
+                                                id: item.id,
+                                                name: item.data().name,
+                                                description: item.data().description,
+                                                count: item.data().count
+                                            });
+                                        }
+                                    });
+                                });
+                                subscription.unsubscribe();
+                                resolve(res);
+                            },
+                            (error) => {
+                                reject(error);
+                            }
+                        );
+                    },
+                    (error) => {
+                        reject(error);
+                    }
+                );
+        });
     }
 
-    add(id, args = null) {
+    getAvailable(id): Promise<any[]> {
+        return new Promise((resolve, reject) => {
+            this.afs.collection('products').doc(id).get()
+                .subscribe(
+                    (next) => {
+                        const subscription = this.afs.collection<Category>('categories').get().subscribe(
+                            (data) => {
+                                const res = [];
+                                console.log(data.docs);
+                                data.docs.forEach((item) => {
+                                    let found = false;
+                                    next.data().categories.forEach((categoryId) => {
+                                        if (item.id === categoryId) {
+                                            found = true;
+                                        }
+                                    });
+                                    if (!found) {
+                                        res.push({
+                                            id: item.id,
+                                            name: item.data().name,
+                                            description: item.data().description,
+                                            count: item.data().count
+                                        });
+                                    }
+                                });
 
+                                subscription.unsubscribe();
+                                resolve(res);
+                            },
+                            (error) => {
+                                reject(error);
+                            }
+                        );
+                    },
+                    (error) => {
+                        reject(error);
+                    }
+                );
+        });
+    }
+
+    add(id, categoryId) {
         return this.afs.collection('products').doc(id).set({
-            'sdfsdfsdf': 1
+            categories: firebase.firestore.FieldValue.arrayUnion(categoryId)
         }, {
             merge: true
         });
     }
 
-    delete(id) {
-        return this.afs.collection('product_categories').doc(id).delete();
+    delete(id, categoryId) {
+        return this.afs.collection('products').doc(id).set({
+            categories: firebase.firestore.FieldValue.arrayRemove(categoryId)
+        }, {
+            merge: true
+        });
     }
 }
