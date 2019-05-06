b12090522e023f309eb7b0620b61eb4bdbde30ba
jacobYousif <jy222bz@student.lnu.se>
2019-05-06 17:32:09 +0200

    + add duration component

--- src/app/components.module.ts
+++ src/app/components.module.ts
@@ -2,18 +2,21 @@ import {NgModule} from '@angular/core';
 import {AlertComponent} from "./shared/components/alert/alert.component";
 import {PageNotFoundComponent} from "./shared/components/page-not-found/page-not-found.component";
 import {CounterComponent} from "./shared/components/counter/counter.component";
+import {DurationComponent} from "./shared/components/duration/duration.component";
 
 @NgModule({
     declarations: [
         AlertComponent,
         PageNotFoundComponent,
         CounterComponent,
+        DurationComponent,
     ],
     imports: [],
     exports: [
         AlertComponent,
         PageNotFoundComponent,
         CounterComponent,
+        DurationComponent,
     ]
 })
 export class ComponentsModule {
