ad0cc1f0cf6696f5cc9bedfb20dfc37e93334cd2
legend_mcdonald <da222pa@student.lnu.se>
2019-05-17 12:11:06 +0200

    deal

---
+++ src/app/modules/main/home/components/deal/deal.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-main-deal',
+    templateUrl: './deal.component.html',
+    styleUrls: ['./deal.component.scss'],
+})
+export class DealComponent implements OnInit {
+
+
+    constructor() {
+
+    }
+
+    ngOnInit(): void {
+    }
+
+
+
+}
+
+
